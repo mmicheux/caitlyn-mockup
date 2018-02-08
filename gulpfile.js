494bf39bdaa4960f4e8f369c3bc1c5585c688aea
Molly Underwood <underwood3mb@gmail.com>
2018-02-08 18:30:50 -0500

    Move index.html to root, remove css files and set up scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('sass', () => {
-  console.log('hola');
+gulp.task('sass', function(){
+  return gulp.src('docs/scss/styles.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('docs/css'))
 });
