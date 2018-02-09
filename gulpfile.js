36c707c7c70a8b685674c1abebe3215ba52d4d7b
Molly Underwood <underwood3mb@gmail.com>
2018-02-08 22:54:02 -0500

    Add BEM mixins, add color variables, style spend section

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
 
-gulp.task('sass', function(){
-  return gulp.src('docs/scss/styles.scss')
-    .pipe(sass()) // Using gulp-sass
+gulp.task('sass', function() {
+  return gulp.src('docs/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
     .pipe(gulp.dest('docs/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('docs/scss/**/*.scss', ['sass']); 
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('docs/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+})
